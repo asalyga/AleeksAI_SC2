ce15b7a3e57b7366d78e2d2901585f7ec7b1c11b
Aleksander Sałyga <59572298+asalyga@users.noreply.github.com>
2020-04-25 10:34:19 +0200

    Added Scouting, Vision

--- AleeksSC2Ai.py
+++ AleeksSC2Ai.py
@@ -1,6 +1,6 @@
 import sc2
 import random
-from sc2 import run_game, maps, Race, Difficulty
+from sc2 import run_game, maps, Race, Difficulty, position
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.ability_id import AbilityId
 from sc2.ids.upgrade_id import UpgradeId
@@ -12,7 +12,6 @@ from sc2.ids.buff_id import BuffId
 from sc2.player import Bot, Computer
 import cv2 as cv2
 import numpy as np
-
 
 
 class AleeksBot(sc2.BotAI):
@@ -29,34 +28,17 @@ class AleeksBot(sc2.BotAI):
         await self.build_pylons()
         await self.build_assimilators()
         await self.expand()
-        await self.build_gateway_core()
-        await self.build_stalker()
+        await self.build_gateway_core_robo()
+        await self.build_army()
         #await self.build_zealot()
         await self.attack()
         await self.chronoboost()
-        await self.intel()
-
-    async def scout(self):
-        if len(self.units(UnitTypeId.OBSERVER)) > 0:
-            scout = self.units(UnitTypeId.OBSERVER)[0]
-            if scout.is_idle:
-                enemy_location = self.enemy_start_locations[0]
-                move_to = enemy_location.random
-                print(move_to)
-                scout.move(move_to)
-
-        else:
-            for rf in self.structures(UnitTypeId.ROBOTICSFACILITY).ready.idle:
-                if self.can_afford(UnitTypeId.OBSERVER) and self.supply_left > 0:
-                    rf.train(UnitTypeId.OBSERVER)
+        await self.vision()
+        await self.scout()
 
 
-
-
-
-    async def intel(self):
+    async def vision(self):
         game_data = np.zeros((self.game_info.map_size[1],self.game_info.map_size[0], 3), np.uint8)
-
         draw_dic = {
             UnitTypeId.NEXUS : [15, (0, 255, 0)],
             UnitTypeId.PYLON : [3, (20, 235, 0)],
@@ -65,20 +47,80 @@ class AleeksBot(sc2.BotAI):
             UnitTypeId.GATEWAY : [3, (200, 100, 0)],
             UnitTypeId.CYBERNETICSCORE : [3, (150, 150, 0)],
             UnitTypeId.STARGATE : [5, (255, 0, 0)],
+            UnitTypeId.ROBOTICSFACILITY : [5, (215, 155, 0)],
             UnitTypeId.VOIDRAY : [3, (255, 100, 0)],
         }
-
-
 
         for unit_type in draw_dic:
            for unit in self.units(unit_type).ready or self.structures(unit_type).ready:
                 pos = unit.position
                 cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dic[unit_type][0], draw_dic[unit_type][1], -1)
 
+        main_base_names = ["nexus", "supplydepot", "hatchery"]
+        for enemy_building in self.enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() not in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 5, (200, 50, 212), -1)
+
+        for enemy_building in self.enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 15, (255, 0, 0), -1)
+
+        for enemy_unit in self.enemy_units:
+            if not enemy_unit.is_structure:
+                worker_names = ["probe",
+                                "scv",
+                                "drone"]
+                pos = enemy_unit.position
+                if enemy_unit.name.lower() in worker_names:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (55, 0, 155), -1)
+                else:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (50, 0, 255), -1)
+
+        for observer in self.units(UnitTypeId.OBSERVER).ready:
+            pos = observer.position
+            cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (225, 225, 225), -1)
+
         flipped = cv2.flip(game_data, 0)
         resized = cv2.resize(flipped, dsize = None, fx = 3, fy = 3)
-        cv2.imshow('Intel', resized)
+        cv2.imshow('vision', resized)
         cv2.waitKey(1)
+
+
+    def random_location(self, enemy_start_locations):
+        x = enemy_start_locations[0]
+        y = enemy_start_locations[1]
+
+        x += ((random.randrange(-20,20))/100) * enemy_start_locations[0]
+        y += ((random.randrange(-20,20))/100) * enemy_start_locations[1]
+
+        if x < 0:
+            x = 0
+        if y < 0:
+            y = 0
+        if x > self.game_info.map_size[0]:
+            x = self.game_info.map_size[0]
+        if y > self.game_info.map_size[1]:
+            y = self.game_info.map_size[1]
+        
+        go_to = position.Point2(position.Pointlike((x,y)))
+        return go_to
+
+    
+    async def scout(self):
+        if len(self.units(UnitTypeId.OBSERVER)) > 0:
+            scout = self.units(UnitTypeId.OBSERVER)[0]
+            if scout.is_idle:
+                enemy_location = self.enemy_start_locations[0]
+                move_to = self.random_location(enemy_location)
+                print(move_to)
+                scout.move(move_to)
+
+        else:
+            for rf in self.structures(UnitTypeId.ROBOTICSFACILITY).ready.idle:
+                if self.can_afford(UnitTypeId.OBSERVER) and self.supply_left > 0:
+                    rf.train(UnitTypeId.OBSERVER)
 
 
     async def build_workers(self):
@@ -116,37 +158,50 @@ class AleeksBot(sc2.BotAI):
 
 
     async def expand(self):
-        if self.townhalls.amount  < 3 and not self.already_pending(UnitTypeId.NEXUS):
+        if self.townhalls.amount  < 3 and not self.already_pending(UnitTypeId.NEXUS) and self.can_afford(UnitTypeId.NEXUS):
             if self.can_afford(UnitTypeId.NEXUS):
                 await self.expand_now()
 
 
-    async def build_gateway_core(self):
+    async def build_gateway_core_robo(self):
         if self.structures(UnitTypeId.PYLON).ready.exists:
             pylon1 = self.structures(UnitTypeId.PYLON).ready.random
+
+
             if self.structures(UnitTypeId.GATEWAY).ready.exists and not self.structures(UnitTypeId.CYBERNETICSCORE):
                 if self.can_afford(UnitTypeId.CYBERNETICSCORE) and not self.already_pending(UnitTypeId.CYBERNETICSCORE):
                     await self.build(UnitTypeId.CYBERNETICSCORE, near = pylon1)
+
 
             elif len(self.structures(UnitTypeId.GATEWAY)) < 1:
                 if self.can_afford(UnitTypeId.GATEWAY) and not self.already_pending(UnitTypeId.GATEWAY):
                     await self.build(UnitTypeId.GATEWAY, near = pylon1)
 
+
             if self.structures(UnitTypeId.CYBERNETICSCORE).ready.exists:
-                if len(self.units(UnitTypeId.STARGATE)) < (self.iteration/self.ITERATION_PER_MINUTE):
-                    if self.can_afford(UnitTypeId.STARGATE) and not self.already_pending(UnitTypeId.STARGATE):
-                        await self.build(UnitTypeId.STARGATE, near = pylon1)
+                if len(self.units(UnitTypeId.STARGATE)) < 5:
+                    if len(self.units(UnitTypeId.STARGATE)) < ((self.iteration*3)/self.ITERATION_PER_MINUTE):
+                        if self.can_afford(UnitTypeId.STARGATE) and not self.already_pending(UnitTypeId.STARGATE):
+                            await self.build(UnitTypeId.STARGATE, near = pylon1)
 
 
-    async def build_stalker(self):
-        #for gateway1 in self.structures(UnitTypeId.GATEWAY).ready.idle:
-        #    if not self.units(UnitTypeId.STALKER).amount > self.units(UnitTypeId.VOIDRAY).amount:
-        #        if self.can_afford(UnitTypeId.STALKER) and self.supply_left > 2:
-        #            gateway1.train(UnitTypeId.STALKER)
+            if self.structures(UnitTypeId.CYBERNETICSCORE).ready.exists:
+                if len(self.structures(UnitTypeId.ROBOTICSFACILITY)) < 1:
+                    if self.can_afford(UnitTypeId.ROBOTICSFACILITY) and not self.already_pending(UnitTypeId.ROBOTICSFACILITY):
+                        await self.build(UnitTypeId.ROBOTICSFACILITY, near = pylon1)
 
-        for stargate in self.structures(UnitTypeId.STARGATE).ready.idle:
-            if self.can_afford(UnitTypeId.VOIDRAY) and self.supply_left > 0:
-                stargate.train(UnitTypeId.VOIDRAY)
+
+    async def build_army(self):
+        if self.units(UnitTypeId.STALKER).amount < 13:
+            for gateway1 in self.structures(UnitTypeId.GATEWAY).ready.idle:
+                #if not self.units(UnitTypeId.STALKER).amount > self.units(UnitTypeId.VOIDRAY).amount:
+                    if self.can_afford(UnitTypeId.STALKER) and self.supply_left > 2:
+                        gateway1.train(UnitTypeId.STALKER)
+
+        if self.units(UnitTypeId.VOIDRAY).amount < 10:
+            for stargate in self.structures(UnitTypeId.STARGATE).ready.idle:
+                if self.can_afford(UnitTypeId.VOIDRAY) and self.supply_left > 0:
+                    stargate.train(UnitTypeId.VOIDRAY)
 
 
     #async def build_zealot(self):
@@ -168,35 +223,24 @@ class AleeksBot(sc2.BotAI):
 
     async def attack(self):
         # {UNIT: [n to fight, n to defend]}
-        aggressive_units = {
-                            #UnitTypeId.STALKER: [15, 1],
+        units = {
+                            UnitTypeId.STALKER: [10, 1],
                             UnitTypeId.VOIDRAY: [8, 1],
                             }
 
-
-        for UNIT in aggressive_units:
-            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+        for UNIT in units:
+            if self.units(UNIT).amount > units[UNIT][0] and self.units(UNIT).amount > units[UNIT][1]:
                 for s in self.units(UNIT).idle:
                     self.do(s.attack(self.find_enemy(self.state)))
-                    #await self.do(s.attack(self.find_enemy(self.state)))
+                    #s.attack(self.find_enemy(self.state))
 
-            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+            elif self.units(UNIT).amount > units[UNIT][1]:
                 if len(self.enemy_units) > 0:
                     for s in self.units(UNIT).idle:
                         self.do(s.attack(random.choice(self.enemy_units)))
-                        #await self.do(s.attack(random.choice(self.known_enemy_units)))
+                        #s.attack(random.choice(self.enemy_units))
 
-    #async def attack(self):
-    #    if self.units(UnitTypeId.STALKER).amount > 15:
-    #        for s in self.units(UnitTypeId.STALKER).idle:
-    #            s.attack(self.find_enemy(self.state))
-
-    #    if self.units(UnitTypeId.STALKER).amount > 5:
-    #        if len(self.enemy_units) > 0:
-    #            for s in self.units(UnitTypeId.STALKER).idle:
-    #                s.attack(random.choice(self.enemy_units))
-            
-                
+              
     async def chronoboost(self):
         nexus = self.townhalls.ready.random
         if not nexus.is_idle and not nexus.has_buff(BuffId.CHRONOBOOSTENERGYCOST):
