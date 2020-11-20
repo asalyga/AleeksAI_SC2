5ee5cd84b76b0cafce569f62db56fcf9bbe53d55
Aleksander Sałyga <59572298+asalyga@users.noreply.github.com>
2020-11-20 14:03:25 +0100

    Bugs Fixes

--- AleeksSC2Ai.py
+++ AleeksSC2Ai.py
@@ -30,10 +30,7 @@ class AleeksBot(sc2.BotAI):
         self.title = title
         self.do_something_after = 0
         self.scouts_and_spots = {}
-        self.train_data = []
-        if self.use_model:
-            print("Using model")
-            self.model = keras.models.load_model("TrainedData")
+
 
         self.choices = {
             0: self.build_scout,
@@ -52,11 +49,18 @@ class AleeksBot(sc2.BotAI):
             13: self.do_nothing,
         }
 
+        self.train_data = []
+        if self.use_model:
+            print("Using model")
+            self.model = keras.models.load_model("TrainedData")
+
+
     def on_end(self, game_result):
         print('--- on_end called ---')
         print(game_result, self.use_model)
-        if game_result == game_result.Victory:
-            np.save("train_data/{}.npy".format(str(int(time.time()))), np.array(self.train_data))
+
+        #if game_result == game_result.Victory:
+        #    np.save("train_data/{}.npy".format(str(int(time.time()))), np.array(self.train_data))
 
         with open("gameout-random-vs-medium.txt", "a") as f:
             if self.use_model:
@@ -64,7 +68,9 @@ class AleeksBot(sc2.BotAI):
             else:
                 f.write("Random {}\n".format(game_result))
 
+
     async def on_step(self, interation):
+
         self.GameTime = (self.state.game_loop / 22.4) / 60
         print('Time:', self.GameTime)
         await self.distribute_workers()
@@ -74,6 +80,172 @@ class AleeksBot(sc2.BotAI):
         await self.chronoboost()
 
 
+    def random_location(self, location):
+        x = location[0]
+        y = location[1]
+
+        x += random.randrange(-5, 5)
+        y += random.randrange(-5, 5)
+
+        if x < 0:
+            x = 0
+        if y < 0:
+            y = 0
+        if x > self.game_info.map_size[0]:
+            x = self.game_info.map_size[0]
+
+        if y > self.game_info.map_size[1]:
+            y = self.game_info.map_size[1]
+
+        go_to = position.Point2(position.Pointlike((x, y)))
+        return go_to
+
+    async def scout(self):
+        self.expand_dis_dir = {}
+
+        for el in self.expansion_locations:
+            distance_to_enemy_start = el.distance_to(self.enemy_start_locations[0])
+            self.expand_dis_dir[distance_to_enemy_start] = el
+
+        self.ordered_exp_distances = sorted(k for k in self.expand_dis_dir)
+
+        existing_ids = [unit.tag for unit in self.units]
+        to_be_removed = []
+        for noted_scout in self.scouts_and_spots:
+            if noted_scout not in existing_ids:
+                to_be_removed.append(noted_scout)
+
+        for scout in to_be_removed:
+            del self.scouts_and_spots[scout]
+
+        if len(self.structures(UnitTypeId.ROBOTICSFACILITY).ready) == 0:
+            unit_type = UnitTypeId.PROBE
+            unit_limit = 1
+        else:
+            unit_type = UnitTypeId.OBSERVER
+            unit_limit = 3
+
+        assign_scout = True
+
+        if unit_type == UnitTypeId.PROBE:
+            for unit in self.units(UnitTypeId.PROBE):
+                if unit.tag in self.scouts_and_spots:
+                    assign_scout = False
+
+        if assign_scout:
+            if len(self.units(unit_type).idle) > 0:
+                for obs in self.units(unit_type).idle[:unit_limit]:
+                    if obs.tag not in self.scouts_and_spots:
+                        for dist in self.ordered_exp_distances:
+                            try:
+                                location = next(value for key, value in self.expand_dis_dir.items() if key == dist)
+                                active_locations = [self.scouts_and_spots[k] for k in self.scouts_and_spots]
+
+                                if location not in active_locations:
+                                    if unit_type == UnitTypeId.PROBE:
+                                        for unit in self.units(UnitTypeId.PROBE):
+                                            if unit.tag in self.scouts_and_spots:
+                                                continue
+                                    await self.do(obs.move(location))
+                                    self.scouts_and_spots[obs.tag] = location
+                                    break
+                            except Exception as e:
+                                pass
+
+        for obs in self.units(unit_type):
+            if obs.tag in self.scouts_and_spots:
+                if obs in [probe for probe in self.units(UnitTypeId.PROBE)]:
+                    obs.move(self.random_location(self.scouts_and_spots[obs.tag]))
+
+    async def vision(self):
+        game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0], 3), np.uint8)
+
+        draw_dic = {
+            UnitTypeId.NEXUS: [5, (0, 255, 0)],
+            UnitTypeId.PYLON: [2, (20, 235, 0)],
+            UnitTypeId.PROBE: [1, (55, 200, 0)],
+            UnitTypeId.ASSIMILATOR: [2, (55, 200, 0)],
+            UnitTypeId.GATEWAY: [3, (200, 100, 0)],
+            UnitTypeId.CYBERNETICSCORE: [3, (150, 150, 0)],
+            UnitTypeId.STARGATE: [3, (255, 0, 0)],
+            UnitTypeId.ROBOTICSFACILITY: [3, (215, 155, 0)],
+            UnitTypeId.VOIDRAY: [2, (255, 100, 0)],
+        }
+
+        for unit_type in draw_dic:
+            for unit in self.units(unit_type).ready or self.structures(unit_type).ready:
+                pos = unit.position
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dic[unit_type][0], draw_dic[unit_type][1], -1)
+
+        main_base_names = ['nexus', 'commandcenter', 'orbitalcommand', 'planetaryfortress', 'hatchery']
+        for enemy_building in self.enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() not in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 5, (200, 50, 212), -1)
+
+        for enemy_building in self.enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 15, (255, 0, 0), -1)
+
+        for enemy_unit in self.enemy_units:
+            if not enemy_unit.is_structure:
+                worker_names = ["probe",
+                                "scv",
+                                "drone"]
+                pos = enemy_unit.position
+                if enemy_unit.name.lower() in worker_names:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (55, 0, 155), -1)
+                else:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (50, 0, 255), -1)
+
+        for observer in self.units(UnitTypeId.OBSERVER).ready:
+            pos = observer.position
+            cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (225, 225, 225), -1)
+
+        for vr in self.units(UnitTypeId.VOIDRAY).ready:
+            pos = vr.position
+            cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (255, 100, 0), -1)
+
+        line_max = 50
+        mineral_ratio = self.minerals / 1500
+        if mineral_ratio > 1.0:
+            mineral_ratio = 1.0
+
+        vespene_ratio = self.vespene / 1500
+        if vespene_ratio > 1.0:
+            vespene_ratio = 1.0
+
+        population_ratio = self.supply_left / self.supply_cap
+        if population_ratio > 1.0:
+            population_ratio = 1.0
+
+        plausible_supply = self.supply_cap / 200.0
+
+        military_weight = len(self.units(UnitTypeId.VOIDRAY)) / (self.supply_cap - self.supply_left)
+        if military_weight > 1.0:
+            military_weight = 1.0
+
+        cv2.line(game_data, (0, 19), (int(line_max * military_weight), 19), (250, 250, 200), 3)
+        cv2.line(game_data, (0, 13), (int(line_max * plausible_supply), 13), (220, 200, 200), 3)
+        cv2.line(game_data, (0, 11), (int(line_max * population_ratio), 11), (150, 150, 150), 3)
+        cv2.line(game_data, (0, 7), (int(line_max * vespene_ratio), 7), (210, 200, 0), 3)
+        cv2.line(game_data, (0, 3), (int(line_max * mineral_ratio), 3), (0, 255, 25), 3)
+
+        self.flipped = cv2.flip(game_data, 0)
+        resized = cv2.resize(self.flipped, dsize=None, fx=3, fy=3)
+
+        if not HEADLESS:
+            cv2.imshow(str(self.title), resized)
+            cv2.waitKey(1)
+
+    def find_enemy(self, state):
+        if len(self.enemy_units) > 0:
+            return random.choice(self.enemy_units)
+        elif len(self.enemy_structures) > 0:
+            return random.choice(self.enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
 
 
     async def build_scout(self):
@@ -228,142 +400,9 @@ class AleeksBot(sc2.BotAI):
     async def distribute_workers(self):
         pass
 
-    async def scout(self):
-        self.expand_dis_dir = {}
+    
 
-        for el in self.expansion_locations:
-            distance_to_enemy_start = el.distance_to(self.enemy_start_locations[0])
-            self.expand_dis_dir[distance_to_enemy_start] = el
-
-        self.ordered_exp_distances = sorted(k for k in self.expand_dis_dir)
-
-        existing_ids = [unit.tag for unit in self.units]
-        to_be_removed = []
-        for noted_scout in self.scouts_and_spots:
-            if noted_scout not in existing_ids:
-                to_be_removed.append(noted_scout)
-
-        for scout in to_be_removed:
-            del self.scouts_and_spots[scout]
-
-        if len(self.structures(UnitTypeId.ROBOTICSFACILITY).ready) == 0:
-            unit_type = UnitTypeId.PROBE
-            unit_limit = 1
-        else:
-            unit_type = UnitTypeId.OBSERVER
-            unit_limit = 3
-
-        assign_scout = True
-
-        if unit_type == UnitTypeId.PROBE:
-            for unit in self.units(UnitTypeId.PROBE):
-                if unit.tag in self.scouts_and_spots:
-                    assign_scout = False
-
-        if assign_scout:
-            if len(self.units(unit_type).idle) > 0:
-                for obs in self.units(unit_type).idle[:unit_limit]:
-                    if obs.tag not in self.scouts_and_spots:
-                        for dist in self.ordered_exp_distances:
-                            try:
-                                location = next(value for key, value in self.expand_dis_dir.items() if key == dist)
-                                active_locations = [self.scouts_and_spots[k] for k in self.scouts_and_spots]
-
-                                if location not in active_locations:
-                                    if unit_type == UnitTypeId.PROBE:
-                                        for unit in self.units(UnitTypeId.PROBE):
-                                            if unit.tag in self.scouts_and_spots:
-                                                continue
-                                    await self.do(obs.move(location))
-                                    self.scouts_and_spots[obs.tag] = location
-                                    break
-                            except Exception as e:
-                                pass
-
-    async def vision(self):
-        game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0], 3), np.uint8)
-        draw_dic = {
-            UnitTypeId.NEXUS: [5, (0, 255, 0)],
-            UnitTypeId.PYLON: [2, (20, 235, 0)],
-            UnitTypeId.PROBE: [1, (55, 200, 0)],
-            UnitTypeId.ASSIMILATOR: [2, (55, 200, 0)],
-            UnitTypeId.GATEWAY: [3, (200, 100, 0)],
-            UnitTypeId.CYBERNETICSCORE: [3, (150, 150, 0)],
-            UnitTypeId.STARGATE: [3, (255, 0, 0)],
-            UnitTypeId.ROBOTICSFACILITY: [3, (215, 155, 0)],
-            UnitTypeId.VOIDRAY: [2, (255, 100, 0)],
-        }
-
-        for unit_type in draw_dic:
-            for unit in self.units(unit_type).ready or self.structures(unit_type).ready:
-                pos = unit.position
-                cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dic[unit_type][0], draw_dic[unit_type][1], -1)
-
-        main_base_names = ['nexus', 'commandcenter', 'orbitalcommand', 'planetaryfortress', 'hatchery']
-        for enemy_building in self.enemy_structures:
-            pos = enemy_building.position
-            if enemy_building.name.lower() not in main_base_names:
-                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 5, (200, 50, 212), -1)
-
-        for enemy_building in self.enemy_structures:
-            pos = enemy_building.position
-            if enemy_building.name.lower() in main_base_names:
-                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 15, (255, 0, 0), -1)
-
-        for enemy_unit in self.enemy_units:
-            if not enemy_unit.is_structure:
-                worker_names = ["probe",
-                                "scv",
-                                "drone"]
-                pos = enemy_unit.position
-                if enemy_unit.name.lower() in worker_names:
-                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (55, 0, 155), -1)
-                else:
-                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (50, 0, 255), -1)
-
-        for observer in self.units(UnitTypeId.OBSERVER).ready:
-            pos = observer.position
-            cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (225, 225, 225), -1)
-
-        for vr in self.units(UnitTypeId.VOIDRAY).ready:
-            pos = vr.position
-            cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (255, 100, 0), -1)
-
-        line_max = 50
-        mineral_ratio = self.minerals / 1500
-        if mineral_ratio > 1.0:
-            mineral_ratio = 1.0
-
-        vespene_ratio = self.vespene / 1500
-        if vespene_ratio > 1.0:
-            vespene_ratio = 1.0
-
-        population_ratio = self.supply_left / self.supply_cap
-        if population_ratio > 1.0:
-            population_ratio = 1.0
-
-        plausible_supply = self.supply_cap / 200.0
-
-        military_weight = len(self.units(UnitTypeId.VOIDRAY)) / (self.supply_cap - self.supply_left)
-        if military_weight > 1.0:
-            military_weight = 1.0
-
-        cv2.line(game_data, (0, 19), (int(line_max * military_weight), 19), (250, 250, 200), 3)
-        cv2.line(game_data, (0, 13), (int(line_max * plausible_supply), 13), (220, 200, 200), 3)
-        cv2.line(game_data, (0, 11), (int(line_max * population_ratio), 11), (150, 150, 150), 3)
-        cv2.line(game_data, (0, 7), (int(line_max * vespene_ratio), 7), (210, 200, 0), 3)
-        cv2.line(game_data, (0, 3), (int(line_max * mineral_ratio), 3), (0, 255, 25), 3)
-
-        self.flipped = cv2.flip(game_data, 0)
-        resized = cv2.resize(self.flipped, dsize=None, fx=3, fy=3)
-
-        if not HEADLESS:
-            if self.use_model:
-                cv2.imshow(str(self.title), resized)
-                cv2.waitKey(1)
-            else:
-                cv2.imshow(str(self.title), resized)
-                cv2.waitKey(1)
+    
 
     async def do_something(self):
         if self.GameTime > self.do_something_after:
@@ -391,33 +430,9 @@ class AleeksBot(sc2.BotAI):
                         loop_nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus)
                         break
 
-    def random_location(self, location):
-        x = location[0]
-        y = location[1]
+    
 
-        x += random.randrange(-5, 5)
-        y += random.randrange(-5, 5)
-
-        if x < 0:
-            x = 0
-        if y < 0:
-            y = 0
-        if x > self.game_info.map_size[0]:
-            x = self.game_info.map_size[0]
-
-        if y > self.game_info.map_size[1]:
-            y = self.game_info.map_size[1]
-
-        go_to = position.Point2(position.Pointlike((x, y)))
-        return go_to
-
-    def find_enemy(self, state):
-        if len(self.enemy_units) > 0:
-            return random.choice(self.enemy_units)
-        elif len(self.enemy_structures) > 0:
-            return random.choice(self.enemy_structures)
-        else:
-            return self.enemy_start_locations[0]
+    
 
 
 map_pool = [
